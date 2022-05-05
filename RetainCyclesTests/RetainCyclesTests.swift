861a19a8554ae13c66c6d52ac21e17d7a8132851
Emma Walker <walkere@tcd.ie>
2022-05-05 14:52:59 +0100

    add a test for case two memory cycle

--- RetainCyclesTests/RetainCyclesTests.swift
+++ RetainCyclesTests/RetainCyclesTests.swift
@@ -2,11 +2,25 @@ import XCTest
 @testable import RetainCycles
 
 class RetainCyclesTests: XCTestCase {
+    
+    
+    
     func testCaseOneRetainCycles() {
         
     }
     
     func testCaseTwoRetainCycles() {
+        //Given - we have a Case two VC which has a C2 presenter on it.
+        let presenter = CaseTwoPresenter()
+        let viewController = CaseTwoViewController(presenter: presenter)
+        //when - Case 2 VC dies
+        addTeardownBlock { [weak presenter, weak viewController] in
+            XCTAssertNil(viewController)
+            XCTAssertNil(presenter)
+        }
+        //then - the presenter is also de allocated.
+        
+        
         
     }
     
