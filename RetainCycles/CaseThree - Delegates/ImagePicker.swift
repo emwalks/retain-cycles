4e4496da68958439199b492c9c0fe007c9088f32
Emma Walker <walkere@tcd.ie>
2022-07-01 13:46:43 +0100

    Set the VC to weak so it is cleaned up appropriately

--- RetainCycles/CaseThree - Delegates/ImagePicker.swift
+++ RetainCycles/CaseThree - Delegates/ImagePicker.swift
@@ -2,7 +2,7 @@ import UIKit
 import Foundation
 
 class ImagePicker: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    private var viewController: UIViewController
+    private weak var viewController: UIViewController?
     var picker: UIImagePickerController
     
     init(viewController: UIViewController) {
@@ -15,12 +15,15 @@ class ImagePicker: NSObject, UIImagePickerControllerDelegate, UINavigationContro
     }
     
     func pickPhoto() {
-        viewController.present(picker, animated: true)
+        viewController?.present(picker, animated: true)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         picker.dismiss(animated: true) {
-            self.viewController.dismiss(animated: true)
+            self.viewController?.dismiss(animated: true)
+            // When the VC was made weak, it had to become optional
+            // So you could also set the view controller to nil here and the test would pass
+            // BUT... tbf weak is what you would expect in Apple Land
         }
         
     }
